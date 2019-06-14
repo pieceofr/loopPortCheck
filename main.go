fe1df5231970eebbbb787ea0a23faf957894b01e
rachael <komimi.p@gmail.com>
2019-06-14 17:15:05 +0800

    change fmt to log

--- main.go
+++ main.go
@@ -27,9 +27,10 @@ func main() {
 	var mode = flag.Int("mode", 0, "Mode 0:client+server 1:server only 2:client only")
 
 	flag.Parse()
-	log.Println("Detecting host port = ", *host, ":", *port)
+	log.Println("Detecting host port = ", *host, ":", *port, " mode:", *mode)
 
 	if *mode == 0 || *mode == 1 {
+
 		go pingPongServer(*port)
 	}
 
@@ -37,28 +38,28 @@ func main() {
 		go CheckPortReachableRoutine(*host, *port)
 	}
 	for {
-		fmt.Println("Ping Pong Result: ", peerPortReachable)
+		log.Println("Ping Pong Result: ", peerPortReachable)
 		time.Sleep(5 * time.Second)
 	}
 
 }
 
 func pingPongServer(port string) {
+	log.Println("----Running Server----")
 	var listenHost string
-
 	l, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
+		log.Println("Error listening:", err.Error())
 		os.Exit(1)
 	}
 	// Close the listener when the application closes.
 	defer l.Close()
-	fmt.Printf("--- %s listen on %s --- \n", listenHost, port)
+	log.Printf("--- %s listen on %s --- \n", listenHost, port)
 	for {
 		// Listen for an incoming connection.
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
+			log.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
 		go handleRequest(conn)
@@ -70,7 +71,7 @@ func handleRequest(conn net.Conn) {
 	buf := make([]byte, 1024)
 	_, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		log.Println("Error reading:", err.Error())
 	}
 	// Send a response back to person contacting us.
 	conn.Write([]byte("Pong"))
@@ -80,25 +81,25 @@ func handleRequest(conn net.Conn) {
 
 //CheckPortReachableRoutine is a Connection Check Routine
 func CheckPortReachableRoutine(host, port string) {
+	log.Println("----Running Client Routine---")
 	status := make(chan bool)
-	fmt.Printf("CheckPortReachableRoutine:%s:%s\n", host, port)
+	log.Printf("CheckPortReachableRoutine:%s:%s\n", host, port)
 	for {
 		go func(updateStatus chan<- bool) {
 			connected := true
 			for retry := 0; retry < retryTimes; retry++ {
 				connected = connToPort(host, port)
 				if !connected {
-					fmt.Printf("NOT able to connect %s:%s\n", host, port)
+					log.Printf("NOT able to connect %s:%s\n", host, port)
 					time.Sleep(retryDelay)
 				} else {
-					//fmt.Printf("Connect to %s:%s\n", host, port)
+					//log.Printf("Connect to %s:%s\n", host, port)
 					retry = retryTimes + 1
 				}
 			}
 			updateStatus <- connected
 
 		}(status)
-
 		peerPortReachable = <-status
 		time.Sleep(time.Duration(checkInterMs) * time.Millisecond)
 	}
@@ -113,13 +114,13 @@ func connToPort(host, port string) bool {
 		return false
 	} else {
 		conn.Write([]byte("Ping"))
-		fmt.Println("Ping")
+		log.Println("Ping")
 		resp := make([]byte, 1024)
 		lenResp, err := conn.Read(resp)
 		if err != nil {
-			fmt.Println("connToPort Error:", err)
+			log.Println("connToPort Error:", err)
 		}
-		fmt.Println(string(resp[:lenResp]))
+		log.Println(string(resp[:lenResp]))
 		conn.Close()
 		return true
 	}
